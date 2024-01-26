b1fcffc7a503da580cde51ec6b91aa4149f2d346
Lucas Nathan Henrique da Silva <lucas.develop.prog@gmail.com>
2024-01-26 10:44:32 -0300

    feat: Swagger added and setted up

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { NestFactory } from "@nestjs/core";
+import { NestApplication, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from "@nestjs/swagger";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestApplication = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("API Docs")
+    .setDescription("API Documentation for default routes in application")
+    .setVersion("1.0")
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   app.enableCors({
     origin: "*",
     methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
   });
+
   await app.listen(3000);
 }
 bootstrap();
