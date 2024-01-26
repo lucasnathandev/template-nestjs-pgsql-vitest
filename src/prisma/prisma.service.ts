b92cab97b18f76d88b13bc64b92a119576b6ffb7
Lucas Nathan Henrique da Silva <lucas.develop.prog@gmail.com>
2024-01-26 00:38:31 -0300

    fix: uneeded dependencies removed and Logger used on database connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import { Logger } from "@nestjs/common";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger: Logger = new Logger(PrismaService.name);
   async onModuleInit() {
     await this.$connect();
+    this.logger.log("Connected to database");
   }
 }
